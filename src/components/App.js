c27bcc7e45a0bd29fc6d45185518386808640825
Yashaswini <aswiniy68@gmail.com>
2025-04-01 09:53:22 +0530

    one shot

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,21 @@
 
-import React from "react";
+import React,{useState} from "react";
 import './../styles/App.css';
 
 const App = () => {
+const [input,setInput] = useState("")
+
   return (
-    <div>
+    <div style={{backgroundColor:"#73F400"}}>
         {/* Do not remove the main div */}
+        <h1>Parent Component</h1>
+        <p>{input}</p>
+        <div className="parent" style={{backgroundColor:"#C45A19"}}>
+          <h1>Child Component</h1>
+          <div className="child" >
+          <input onChange={(e)=>setInput(e.target.value)}></input>
+          </div>
+        </div>
     </div>
   )
 }
